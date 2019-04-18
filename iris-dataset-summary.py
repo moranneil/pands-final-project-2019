3d0fd939b68a42d6160be85af49d68cc67a91bfc
Neil Moran <G00376338@gmit.ie>
2019-04-18 19:43:11 +0100

    Added comments to include all calculated values, min , max, mean and std

--- iris-dataset-summary.py
+++ iris-dataset-summary.py
@@ -29,7 +29,7 @@ print("********************************")
 #Assign the second column of the dataset to the array septalwidth
 sepalwidth = irisdata[:,1]
 
-#The min, max and average septalwidth is calculated, the mean calculation is rounded to two decimal places.
+#The min, max average and standard devation of the septalwidth is calculated, the mean calculation is rounded to two decimal places.
 minsepalwidth = numpy.min(irisdata[:,1])
 maxsepalwidth = numpy.max(irisdata[:,1])
 meansepalwidth = numpy.mean(irisdata[:,1])
@@ -37,7 +37,7 @@ stdsepalwidth = numpy.std(irisdata[:,1])
 meansepalwidthdec2 = round(meansepalwidth,2)
 stdsepalwidthdec2 = round(stdsepalwidth,2)
 
-#Print the min, max and average sepal width rounded to two decmial places
+#Print the min, max average and standard devation of the sepal width rounded to two decmial places
 print(f"The minimum sepal width is {minsepalwidth}")
 print(f"The maximum sepal width is {maxsepalwidth}")
 print(f"The average sepal width is {meansepalwidthdec2}")
@@ -47,7 +47,7 @@ print("********************************")
 #Assign the third column of the dataset to the array septallength
 petallength = irisdata[:,2]
 
-#The min, max and average of the petallength is calculated, the mean calculation is rounded to two decimal places.
+#The min, max average and standard devation of the petallength is calculated, the mean calculation is rounded to two decimal places.
 minpetallength = numpy.min(irisdata[:,2])
 maxpetallength = numpy.max(irisdata[:,2])
 meanpetallength = numpy.mean(irisdata[:,2])
@@ -55,7 +55,7 @@ stdpetallength = numpy.std(irisdata[:,2])
 meanpetallengthdec2 = round(meanpetallength,2)
 stdpetallengthdec2 = round(stdpetallength,2)
 
-##Print the average petal length rounded to two decmial places
+##Print the min, max average and standard devation of the petal length rounded to two decmial places
 print(f"The minimum petal length is {minpetallength}")
 print(f"The maximum petal length is {maxpetallength}")
 print(f"The average petal length is {meanpetallengthdec2}")
@@ -65,7 +65,7 @@ print("********************************")
 #Assign the fourth column of the dataset to the array petalwidth
 petalwidth = irisdata[:,3]
 
-#The average of the petalwidth is calculated, the mean calculation is rounded to two decimal places.
+#The min, max average and standard devation of the petalwidth is calculated, the mean calculation is rounded to two decimal places.
 minpetalwidth = numpy.min(irisdata[:,3])
 maxpetalwidth = numpy.max(irisdata[:,3])
 meanpetalwidth = numpy.mean(irisdata[:,3])
@@ -74,7 +74,7 @@ meanpetalwidthdec2 = round(meanpetalwidth,2)
 stdpetalwidthdec2 = round(stdpetalwidth,2)
 
 
-#Print the average sepal width rounded to two decmial places
+#Print min, max average and standard devation of the sepal width rounded to two decmial places
 print(f"The minimum petal width is {minpetalwidth}")
 print(f"The maximum petal width is {maxpetalwidth}")
 print(f"The average petal width is {meanpetalwidthdec2}")
