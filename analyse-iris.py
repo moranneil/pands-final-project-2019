1e9e4143d315f85cc9029c01e617fb83e5484800
Neil Moran <G00376338@gmit.ie>
2019-04-19 09:13:55 +0100

    Modified Output of program

--- analyse-iris.py
+++ analyse-iris.py
@@ -12,20 +12,24 @@ irisdataset = pd.read_csv('iris-dataset.csv', delimiter=',', names=['Sepal Lengt
 
 print()
 print()
-print('This is the first five rows in the Iris Dataset') 
+print('This is the first five rows in the Iris Dataset')
+print()
 print(irisdataset.head())
 print()
 print()
 print('Summary Statistics for the entire Iris Dataset')
+print()
 #This command gives a statistical summary of the data that was imported from the CSV file.
 print(irisdataset.describe())
 print()
 print()
 print('Number of rows grouped by column heading')
+print()
 #This command gives statistical info on how many rows in each of the named columns
 print(irisdataset.info())
 print()
 print()
 print('Number of rows grouped by column Species')
+print()
 #This command gives the quantities of samples for each of the values in the column Species
 print(irisdataset['Species'].value_counts())
